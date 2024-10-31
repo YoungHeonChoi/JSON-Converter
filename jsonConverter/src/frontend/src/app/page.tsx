4426fcba029763649d5e8a7a8af97fc69fbf68c4
okerry <gjswns21@gmail.com>
2024-10-31 14:22:58 +0900

    Update FileUpload
    - json파일 내용 markdown형식으로 변환
    - jackson 라이브러리 의존성 추가

--- jsonConverter/src/frontend/src/app/page.tsx
+++ jsonConverter/src/frontend/src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
     if(chkType === "application/json"){
       setFile(e.target.files[0]);
     }else{
-      window.alert("json파일만 업로드 할 수 있습니다.");
+      alert("json파일만 업로드 할 수 있습니다.");
       return;
     }
   };
@@ -40,7 +40,11 @@ export default function Home() {
       } as RequestInit);
 
       if (response.ok) {
+        const markdownContent = await response.text();
+        console.log(markdownContent);
+        downloadMarkdownFile(markdownContent); // 자동으로 다운로드
         alert("업로드 성공");
+        setFile(null);
       } else {
         alert("업로드 실패");
       }
@@ -49,117 +53,129 @@ export default function Home() {
     }
   };
 
+  const downloadMarkdownFile = (content) => {
+    const blob = new Blob([content], { type: 'text/markdown' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'user-profile.md'; // 다운로드할 파일 이름
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url); // 메모리 해제
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-4xl font-bold underline">
-          Json Converter
-        </h1>
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+          <h1 className="text-4xl font-bold underline">
+            Json Converter
+          </h1>
 
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            {" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              .json
-            </code>
-            파일을 업로드하세요
-          </li>
-          <li>
-            {" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              README.md
-            </code>
-            로 추출하거나
-            {" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              Git Repository
-            </code>
-            에
-            {" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              Push
-            </code>
-            하세요.
-          </li>
-        </ol>
+          <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+            <li className="mb-2">
+              {" "}
+              <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+                .json
+              </code>
+              파일을 업로드하세요
+            </li>
+            <li>
+              {" "}
+              <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+                README.md
+              </code>
+              로 추출하거나
+              {" "}
+              <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+                Git Repository
+              </code>
+              에
+              {" "}
+              <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+                Push
+              </code>
+              하세요.
+            </li>
+          </ol>
 
-        <form
-            onSubmit={handleSubmit}
-            className="flex gap-4 items-center flex-col sm:flex-row p-2 border-2 border-dashed border-gray-300 rounded-lg"
-        >
-          <label className="cursor-pointer">
-            <input
-                type="file"
-                onChange={handleFileChange}
-                className="hidden"
-                accept=".json"
-            />
-            <div className="flex flex-col items-center justify-center h-16 w-64 p-4 bg-gray-100 border border-gray-300 rounded-lg text-center text-gray-500 hover:bg-[#383838] dark:hover:bg-[#ccc]">
-              {file ? (
-                  <span className="font-semibold text-gray-700">{file.name}</span>
-              ) : (
-                  <>
-                    <span className="font-semibold text-background">Click to upload a json file</span>
-                    <span className="text-xs mt-1 text-background">or drag and drop</span>
-                  </>
-              )}
-            </div>
-          </label>
-        </form>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="#"
-            rel="noopener noreferrer"
-            onClick={handleSubmit}
+          <form
+              onSubmit={handleSubmit}
+              className="flex gap-4 items-center flex-col sm:flex-row p-2 border-2 border-dashed border-gray-300 rounded-lg"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Export README.md
-          </a>
+            <label className="cursor-pointer">
+              <input
+                  type="file"
+                  onChange={handleFileChange}
+                  className="hidden"
+                  accept=".json"
+              />
+              <div className="flex flex-col items-center justify-center h-16 w-64 p-4 bg-gray-100 border border-gray-300 rounded-lg text-center text-gray-500 hover:bg-[#383838] dark:hover:bg-[#ccc]">
+                {file ? (
+                    <span className="font-semibold text-gray-700">{file.name}</span>
+                ) : (
+                    <>
+                      <span className="font-semibold text-background">Click to upload a json file</span>
+                      <span className="text-xs mt-1 text-background">or drag and drop</span>
+                    </>
+                )}
+              </div>
+            </label>
+          </form>
+
+          <div className="flex gap-4 items-center flex-col sm:flex-row">
+            <a
+                className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+                href="#"
+                rel="noopener noreferrer"
+                onClick={handleSubmit}
+            >
+              <Image
+                  className="dark:invert"
+                  src="/vercel.svg"
+                  alt="Vercel logomark"
+                  width={20}
+                  height={20}
+              />
+              Export README.md
+            </a>
+            <a
+                className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+                href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              <Image
+                  className="dark:invert"
+                  src="/vercel.svg"
+                  alt="Vercel logomark"
+                  width={20}
+                  height={20}
+              />
+              Git Repository Push
+            </a>
+          </div>
+
+        </main>
+
+        <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
           <a
-              className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+              href="https://github.com/YoungHeonChoi/JSON-Converter"
               target="_blank"
               rel="noopener noreferrer"
           >
             <Image
-                className="dark:invert"
-                src="/vercel.svg"
-                alt="Vercel logomark"
-                width={20}
-                height={20}
+                aria-hidden
+                src="/globe.svg"
+                alt="Globe icon"
+                width={16}
+                height={16}
             />
-            Git Repository Push
+            Okerry Github →
           </a>
-        </div>
+        </footer>
 
-      </main>
-
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://github.com/YoungHeonChoi/JSON-Converter"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Okerry Github →
-        </a>
-      </footer>
-
-    </div>
+      </div>
   );
 }
