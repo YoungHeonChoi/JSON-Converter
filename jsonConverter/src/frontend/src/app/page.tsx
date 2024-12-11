bd0a1f7a6e7406c2a94e70029e60f34016e53a90
okerry <gjswns21@gmail.com>
2024-12-11 23:25:11 +0900

    oauth 준비

--- jsonConverter/src/frontend/src/app/page.tsx
+++ jsonConverter/src/frontend/src/app/page.tsx
@@ -1,12 +1,22 @@
+"use client"
+
 import { Github } from "lucide-react";
 import React from "react";
-import Link from "next/link";
 import {Metadata} from "next";
 
-export const metadata: Metadata = {
-    title : "Home",
-};
+// export const metadata: Metadata = {
+//     title : "Home",
+// };
+
 export default function Home() {
+    const redirectToGitHub = () => {
+        const clientId = '';
+        const redirectUri = '';
+        const githubAuthUrl = ``;
+
+        window.location.href = '';
+    };
+
     return (
         <main className="min-h-screen w-full flex items-center justify-center bg-gray-900 px-4">
             <div className="w-full max-w-md bg-gray-800 rounded-lg shadow-md p-8 transform transition-transform hover:scale-[1.02]">
@@ -19,9 +29,10 @@ export default function Home() {
 
                     <button
                         className="w-full bg-gray-700 text-white rounded-md py-3 px-4 flex items-center justify-center gap-3 hover:bg-gray-600 transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
+                        onClick={redirectToGitHub}
                     >
                         <Github className="w-5 h-5" />
-                        <Link href="converter/step1" className="font-medium">GitHub Login</Link>
+                        <p className="font-medium">GitHub Login</p>
                     </button>
                 </div>
             </div>
