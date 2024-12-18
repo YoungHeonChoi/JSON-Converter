1b6ed697e3a6ff96d9a0d291e65df6d56a381ed1
okerry <gjswns21@gmail.com>
2024-12-18 22:00:44 +0900

    token조회 준비 및 code 받아오기

--- jsonConverter/src/frontend/src/app/page.tsx
+++ jsonConverter/src/frontend/src/app/page.tsx
@@ -1,21 +1,59 @@
 "use client"
 
 import { Github } from "lucide-react";
-import React from "react";
+import React, {useEffect} from "react";
+import Cookie from 'js-cookie';
+import axios from 'axios';
 import {Metadata} from "next";
+import { useRouter } from 'next/router';
+import {useSearchParams} from "next/navigation";
 
 // export const metadata: Metadata = {
 //     title : "Home",
 // };
 
 export default function Home() {
+    const searchParams = useSearchParams()
+    const code = searchParams.get('code') // 쿼리 문자열에서 'code' 값 가져오기
+
+    useEffect(() => {
+        if(!code){
+            return;
+        }
+
+        const tokenChk = async () => {
+            try {
+                alert("success");
+                // const response = await fetch('http://localhost:8080/api/your-endpoint', {
+                //     method: 'POST',
+                //     headers: {
+                //         'Content-Type': 'application/json',
+                //     },
+                // });
+                //
+                // if (!response.ok) {
+                //     throw new Error('Failed to fetch data');
+                // }
+                //
+                // const data = await response.json();
+                // console.log('Data from Spring Boot:', data);
+            } catch (error) {
+                // console.error('Error fetching data:', error);
+            }
+        };
+
+        tokenChk();
+    }, []);
+
     const redirectToGitHub = () => {
         const clientId = '';
         const redirectUri = '';
         const githubAuthUrl = ``;
 
-        window.location.href = '';
+        window.location.href = 'https://github.com/login/oauth/authorize?client_id=Ov23liYD7eo0bvhsMMk6&redirect_uri=http://localhost:3000/';
     };
+
+
 
     return (
         <main className="min-h-screen w-full flex items-center justify-center bg-gray-900 px-4">
